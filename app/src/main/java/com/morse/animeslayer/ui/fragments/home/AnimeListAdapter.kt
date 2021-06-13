19a5a786157cd61bae4190b3abab22b7a559618c
mohamed.morse <mohamed.morse@expertapps.com.sa>
2021-06-13 21:43:19 +0200

    setup pop up card

--- app/src/main/java/com/morse/animeslayer/ui/fragments/home/AnimeListAdapter.kt
+++ app/src/main/java/com/morse/animeslayer/ui/fragments/home/AnimeListAdapter.kt
@@ -1,14 +1,21 @@
 package com.morse.animeslayer.ui.fragments.home
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.morse.animeslayer.databinding.AnimeItemLayoutBinding
 import com.morse.animeslayer.domain.AnimeListResponse
+import com.morse.animeslayer.ui.fragments.menu.MenuItem
 
-class AnimeListAdapter : RecyclerView.Adapter<AnimeListAdapter.AnimeViewHolder>() {
+interface AnimeListListener {
+    fun onAnimeClicked ( animeView : View , anime : AnimeListResponse.Anime)
+}
 
-    private val listOfAnime = arrayListOf<AnimeListResponse.Anime>(
+
+class AnimeListAdapter (private val animeListListener: AnimeListListener) : RecyclerView.Adapter<AnimeListAdapter.AnimeViewHolder>() {
+
+    private val listOfAnime = arrayListOf(
         AnimeListResponse.Anime(
             title = "DIGIMON ADVENTURE: 2020",
             imageUrl = "https://images.squarespace-cdn.com/content/v1/596001c2579fb355caec7aac/1593544464864-G6P4BZA1W880KYYO16DA/ke17ZwdGBToddI8pDm48kJtX192gXcg_LgA3lCIbBpNZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpyqMo3jzJhLqXTCrPyqviB5hVyaUuIqL1zNo8v7faVDIjGq_WbF1sCo3nyx4oc98OM/kakeguri_best_anime_characters_cosplay.png?format=500w",
@@ -70,6 +77,9 @@ class AnimeListAdapter : RecyclerView.Adapter<AnimeListAdapter.AnimeViewHolder>(
 
         fun bindAnimeDataToBinding(animeItem: AnimeListResponse.Anime) {
             binding.animeItem = animeItem
+            binding.root.setOnClickListener {
+                animeListListener.onAnimeClicked(binding.root , animeItem)
+            }
         }
 
     }
--- app/src/main/java/com/morse/animeslayer/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/morse/animeslayer/ui/fragments/home/HomeFragment.kt
@@ -1,28 +1,32 @@
 package com.morse.animeslayer.ui.fragments.home
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
+import androidx.annotation.RequiresApi
+import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.google.android.flexbox.FlexDirection
-import com.google.android.flexbox.FlexWrap
-import com.google.android.flexbox.FlexboxLayoutManager
-import com.google.android.flexbox.JustifyContent
+import com.google.android.material.transition.platform.MaterialArcMotion
+import com.google.android.material.transition.platform.MaterialContainerTransform
 import com.morse.animeslayer.R
 import com.morse.animeslayer.databinding.FragmentHomeBinding
+import com.morse.animeslayer.domain.AnimeListResponse
 import com.morse.animeslayer.ui.fragments.menu.MenuBottomSheet
 import com.morse.common.extensions.navigateSafe
 import com.morse.common.utils.ItemOffsetDecoration
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), AnimeListListener {
 
-    private val animeAdapter = AnimeListAdapter()
+    private val animeAdapter = AnimeListAdapter(this)
     val binding: FragmentHomeBinding by lazy {
         FragmentHomeBinding.inflate(layoutInflater)
     }
+    lateinit var animeView: View
 
     private val homeClickListener = View.OnClickListener {
         when (it.id) {
@@ -46,6 +50,51 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onAnimeClicked(animeView: View, anime: AnimeListResponse.Anime) {
+        if (binding.cardOfAnimeDescribtion.visibility == View.VISIBLE) {
+            returnCardToOriginPosition(200)
+        } else {
+            animateCard(animeView)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun returnCardToOriginPosition(duration: Long) {
+        android.transition.TransitionManager.beginDelayedTransition(
+            binding.homeRoot,
+            getTransform(binding.cardOfAnimeDescribtion, animeView, duration)
+        )
+        binding.cardOfAnimeDescribtion?.isGone = true
+        animeView?.isGone = false
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    fun animateCard(view: View) {
+        animeView = view
+        android.transition.TransitionManager.beginDelayedTransition(
+            binding.homeRoot,
+            getTransform(view, binding.cardOfAnimeDescribtion, 650)
+        )
+        view?.isGone = true
+        binding.cardOfAnimeDescribtion?.isGone = false
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun getTransform(
+        mStartView: View,
+        mEndView: View,
+        customDuration: Long
+    ): MaterialContainerTransform {
+        return MaterialContainerTransform().apply {
+            startView = mStartView
+            endView = mEndView
+            addTarget(mEndView)
+            pathMotion = MaterialArcMotion()
+            duration = customDuration
+            scrimColor = Color.TRANSPARENT
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         with(binding) {
@@ -56,7 +105,15 @@ class HomeFragment : Fragment() {
                 homeClickListener
             )
             this.animeListRv.adapter = animeAdapter
-            this.animeListRv.addItemDecoration(ItemOffsetDecoration( requireContext() , R.dimen._5sdp))
+            this.animeListRv.addItemDecoration(
+                ItemOffsetDecoration(
+                    requireContext(),
+                    R.dimen._5sdp
+                )
+            )
+            this.closeCard.setOnClickListener {
+                returnCardToOriginPosition(650)
+            }
         }
     }
 }
