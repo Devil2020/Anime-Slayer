160853bced28a0769bf2765482fda49a75049523
mohamed.morse <mohamed.morse@expertapps.com.sa>
2021-06-15 11:47:36 +0200

    ifference between map , flatmap , concatmap and switchmap

--- app/src/main/java/com/morse/animeslayer/ui/fragments/splash/SplashFragment.kt
+++ app/src/main/java/com/morse/animeslayer/ui/fragments/splash/SplashFragment.kt
@@ -1,16 +1,22 @@
 package com.morse.animeslayer.ui.fragments.splash
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.morse.animeslayer.R
 import com.morse.animeslayer.databinding.FragmentSplashBinding
 import com.morse.common.extensions.navigateSafe
 import com.morse.common.extensions.valueAnimateDescending
+import kotlinx.coroutines.flow.*
 
 class SplashFragment : Fragment() {
 
@@ -47,3 +53,29 @@ class SplashFragment : Fragment() {
     }
 
 }
+
+/*
+    1- What is difference between Map , FlatMap , ConcatMap and SwitchMap ?
+
+                          Map               FlatMap                ConcatMap            SwitchMap
+
+
+exist in collection       yes                yes                      no                     no
+
+exist in Stream           yes                yes                      yes                    yes
+
+purpose           [ change the shape of   [ it used for concat  [ it used for merge      [ Switch Map it is
+                   the value inside it ,   2 Lists into only     2 Streams and the          very difference ,
+                   like from string to    one List so we can     difference is that         it only return the
+                   int or append some     merge 2 lists in one   the data emitted by        last Observable only ]
+                   words to string ]      and in Stream , it     stream will be ordered
+                                          returns another Stream with same order of
+                                          the order may be not   emittion ]
+                                          Sorted with emittion
+                                          time ]
+
+
+
+
+
+*/
